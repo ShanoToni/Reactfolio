a46b1a016e39ee051cdcc424c91b840aecb5abd7
Anton <anton_b_mitkov@abv.bg>
2019-12-14 22:01:52 +0000

    Updates

---
+++ precache-manifest.0e13da6e6677243ced595ad3674e98be.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af7bba2c5b9ba2c48ea718570a1613a2",
+    "url": "/Reactfolio/index.html"
+  },
+  {
+    "revision": "c42d712847f68731f9d9",
+    "url": "/Reactfolio/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "8164f1f175295c60512b",
+    "url": "/Reactfolio/static/css/main.4b6ca76f.chunk.css"
+  },
+  {
+    "revision": "c42d712847f68731f9d9",
+    "url": "/Reactfolio/static/js/2.aab1bb81.chunk.js"
+  },
+  {
+    "revision": "8164f1f175295c60512b",
+    "url": "/Reactfolio/static/js/main.37b0f737.chunk.js"
+  },
+  {
+    "revision": "900660f0a6d0306d701c",
+    "url": "/Reactfolio/static/js/runtime-main.29504123.js"
+  }
+]);
